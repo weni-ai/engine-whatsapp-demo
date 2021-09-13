fa7b6c51828a2d15bfd02b9d59461c3179d273a0
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-13 13:12:46 -0300

    refactor: remove stacktrace from logger

--- logger/logger.go
+++ logger/logger.go
@@ -14,6 +14,7 @@ func init() {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.TimeKey = "timestamp"
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.StacktraceKey = ""
 	config.EncoderConfig = encoderConfig
 
 	log, err = config.Build(zap.AddCallerSkip(1))
