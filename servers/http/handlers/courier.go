a66784c596609a15b2a4c66990d8ac16957fa5b8
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-30 20:34:10 -0300

    refactor: courierHandler to send message with wpp service

--- servers/http/handlers/courier.go
+++ servers/http/handlers/courier.go
@@ -1,54 +1,35 @@
 package handlers
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 
-	"github.com/weni/whatsapp-router/config"
 	"github.com/weni/whatsapp-router/logger"
+	"github.com/weni/whatsapp-router/services"
 )
 
-func HandleSendMessage(w http.ResponseWriter, r *http.Request) {
-	bodyBytes, _ := ioutil.ReadAll(r.Body)
-	bodyString := string(bodyBytes)
-
-	logger.Info(bodyString)
-	PostToWhatsapp(bodyBytes, w)
+type CourierHandler struct {
+	WhatsappService services.WhatsappService
 }
 
-func PostToWhatsapp(body []byte, w http.ResponseWriter) {
-	wconfig := config.GetConfig().Whatsapp
-
-	httpClient := &http.Client{}
-	reqPath := "/v1/messages"
-
-	reqURL, _ := url.Parse(wconfig.BaseURL + reqPath)
-	req := &http.Request{
-		Method: "POST",
-		URL:    reqURL,
-		Header: map[string][]string{
-			"Content-Type":  {"application/json; charset=UTF-8"},
-			"Authorization": {"Bearer " + wconfig.AuthToken},
-		},
-		Body: ioutil.NopCloser(bytes.NewReader(body)),
-	}
-
-	res, err := httpClient.Do(req)
+func (c *CourierHandler) HandleSendMessage(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, _ := ioutil.ReadAll(r.Body)
+	bodyString := string(bodyBytes)
+	logger.Debug(bodyString)
+	header, body, err := c.WhatsappService.SendMessage(bodyBytes)
 
 	if err != nil {
 		logger.Error(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusCreated)
+		fmt.Fprint(w, nil)
+		return
 	}
 
-	b, _ := ioutil.ReadAll(res.Body)
-	for k, v := range res.Header {
+	for k, v := range header {
 		w.Header().Set(k, strings.Join(v, ""))
 	}
-	fmt.Fprint(w, string(b))
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, body)
 }
