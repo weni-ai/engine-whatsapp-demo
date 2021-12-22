a1bc574a48ab27b1ced3211aefcaef67a16943e7
Rafael Soares <rafael.dvlpr@gmail.com>
2021-12-22 19:16:55 -0300

    add update method to contact repository

--- repositories/contact_repository_test.go
+++ repositories/contact_repository_test.go
@@ -17,6 +17,13 @@ var dummyChannel = models.Channel{
 	Token: "foo-bar-zaz",
 }
 
+var dummyChannel2 = models.Channel{
+	ID:    primitive.NewObjectID(),
+	UUID:  "h11c744c-4937-4ee3-8a51-26e56eb77c4e",
+	Name:  "foo2",
+	Token: "foo-bar-zaz2",
+}
+
 var tcInsertContact = []struct {
 	TestName string
 	Contact  models.Contact
@@ -79,6 +86,46 @@ func TestFindOneContact(t *testing.T) {
 			}
 			if c == nil {
 				t.Errorf("got %v / want %v", c, tc.Contact)
+			}
+		})
+	}
+}
+
+var tcUpdateContact = []struct {
+	TestName       string
+	Contact        models.Contact
+	Err            error
+	ChannelToUdate models.Channel
+}{
+	{
+		TestName: "Find one existing contact",
+		Contact: models.Contact{
+			URN:     "5582988887777",
+			Name:    "dummy",
+			Channel: dummyChannel.ID,
+		},
+		ChannelToUdate: dummyChannel2,
+	},
+}
+
+func TestUpdateContact(t *testing.T) {
+	mongodb := storage.NewTestDB()
+	defer storage.CloseDB(mongodb)
+	contactRepository := ContactRepositoryDb{DB: mongodb}
+
+	for _, tc := range tcUpdateContact {
+		t.Run(tc.TestName, func(t *testing.T) {
+			contactToUpdate := &models.Contact{
+				URN:     tc.Contact.URN,
+				Name:    tc.Contact.Name,
+				Channel: tc.ChannelToUdate.ID,
+			}
+			c, err := contactRepository.Update(contactToUpdate)
+			if fmt.Sprint(err) != fmt.Sprint(tc.Err) {
+				t.Errorf("got %v / want %v", err, tc.Err)
+			}
+			if c == nil {
+				t.Errorf("got %v / want %v", c, contactToUpdate)
 			}
 		})
 	}
