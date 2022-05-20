29898ac6765366e49e6b59bd1bc1680974d62f75
Rafael Soares <rafael.dvlpr@gmail.com>
2022-05-20 18:43:52 -0300

    keycloak auth middleware

--- go.mod
+++ go.mod
@@ -3,22 +3,20 @@ module github.com/weni/whatsapp-router
 go 1.16
 
 require (
+	github.com/Nerzal/gocloak/v11 v11.2.0
 	github.com/certifi/gocertifi v0.0.0-20210507211836-431795d63e8d // indirect
 	github.com/evalphobia/logrus_sentry v0.8.2
 	github.com/getsentry/raven-go v0.2.0 // indirect
 	github.com/go-chi/chi v1.5.4
 	github.com/go-co-op/gocron v1.11.0
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/golang/mock v1.6.0
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
 	github.com/klauspost/compress v1.9.7 // indirect
 	github.com/prometheus/client_golang v1.11.0
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.7.1
 	go.mongodb.org/mongo-driver v1.7.2
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20211008194852-3b03d305991f // indirect
-	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
-	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.26.0-rc.1
 )
--- servers/http/handlers/integrations.go
+++ servers/http/handlers/integrations.go
@@ -1,14 +1,20 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
+	"github.com/Nerzal/gocloak/v11"
+	"github.com/weni/whatsapp-router/config"
 	"github.com/weni/whatsapp-router/models"
 	"github.com/weni/whatsapp-router/services"
 	"github.com/weni/whatsapp-router/utils"
 )
+
+var kkClient gocloak.GoCloak
 
 type IntegrationsHandler struct {
 	ChannelService services.ChannelService
@@ -28,4 +34,30 @@ func (h *IntegrationsHandler) HandleCreateChannel(w http.ResponseWriter, r *http
 	}
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf(`{"token":"%s"}`, ch.Token)))
+}
+
+func KeycloackAuth(next http.HandlerFunc) http.HandlerFunc {
+	if kkClient == nil {
+		kkClient = NewKeycloakClient()
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("Authorization")
+		authSplit := strings.Split(authorization, " ")
+		var token string
+		token = authSplit[0]
+		if len(authSplit) > 1 {
+			token = authSplit[1]
+		}
+
+		ctx := context.Background()
+		_, err := kkClient.GetUserInfo(ctx, token, config.GetConfig().OIDC.Realm)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+		}
+		next.ServeHTTP(w, r)
+	}
+}
+
+func NewKeycloakClient() gocloak.GoCloak {
+	return gocloak.NewClient(config.GetConfig().OIDC.Host)
 }
