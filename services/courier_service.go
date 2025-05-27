1572cdce7309fd1d9c93e0505483bc426947daa6
Rafael Soares <rafael.dvlpr@gmail.com>
2025-05-27 16:40:01 -0300

    handle receive and redirect wac incoming

--- services/courier_service.go
+++ services/courier_service.go
@@ -10,6 +10,7 @@ import (
 
 type CourierService interface {
 	RedirectMessage(string, string) (int, error)
+	RedirectMessageWac(string, string) (int, error)
 }
 
 type DefaultCourierService struct {
@@ -17,6 +18,21 @@ type DefaultCourierService struct {
 
 func (cs DefaultCourierService) RedirectMessage(channelUUID string, msg string) (int, error) {
 	courierBaseURL := config.GetConfig().App.CourierBaseURL
+	url := fmt.Sprintf("%v/%v/receive", courierBaseURL, channelUUID)
+	resp, err := http.Post(
+		url,
+		"application/json",
+		bytes.NewBuffer([]byte(msg)))
+
+	if err != nil {
+		return resp.StatusCode, err
+	}
+
+	return resp.StatusCode, nil
+}
+
+func (cs DefaultCourierService) RedirectMessageWac(channelUUID string, msg string) (int, error) {
+	courierBaseURL := config.GetConfig().App.CloudURL
 	url := fmt.Sprintf("%v/%v/receive", courierBaseURL, channelUUID)
 	resp, err := http.Post(
 		url,
