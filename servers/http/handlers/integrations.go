18af5e2d838ec447a84dd4da34a588f0785bd50b
Rafael Soares <rafael.dvlpr@gmail.com>
2025-05-29 18:20:34 -0300

    bypass integrations auth

--- servers/http/handlers/integrations.go
+++ servers/http/handlers/integrations.go
@@ -1,11 +1,9 @@
 package handlers
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/Nerzal/gocloak/v11"
 	"github.com/weni/whatsapp-router/config"
@@ -45,20 +43,20 @@ func KeycloackAuth(next http.HandlerFunc) http.HandlerFunc {
 		kkClient = NewKeycloakClient()
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		authorization := r.Header.Get("Authorization")
-		authSplit := strings.Split(authorization, " ")
-		var token string
-		token = authSplit[0]
-		if len(authSplit) > 1 {
-			token = authSplit[1]
-		}
+		// authorization := r.Header.Get("Authorization")
+		// authSplit := strings.Split(authorization, " ")
+		// var token string
+		// token = authSplit[0]
+		// if len(authSplit) > 1 {
+		// 	token = authSplit[1]
+		// }
 
-		ctx := context.Background()
-		_, err := kkClient.GetUserInfo(ctx, token, config.GetConfig().OIDC.Realm)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusUnauthorized)
-			return
-		}
+		// ctx := context.Background()
+		// _, err := kkClient.GetUserInfo(ctx, token, config.GetConfig().OIDC.Realm)
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusUnauthorized)
+		// 	return
+		// }
 		next.ServeHTTP(w, r)
 	}
 }
